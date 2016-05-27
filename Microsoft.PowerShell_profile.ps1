0bd6f0258e2dd2adb63070e48f0ec834a350de8f
Janus Schmidt <janusschmidt@gmail.com>
2016-05-27 22:37:04 +0200

    Format whitespace

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,29 +4,29 @@ $alias:e = $EDITOR
 function s() {cd ..}
 function touch ($path) 
 {
-	if (-not $path)
+  if (-not $path)
   {
-		"Der skal angives en sti inkl. filnavn og extension"
-	}
-	$dir = [System.IO.Path]::GetDirectoryName($path)
+    "Der skal angives en sti inkl. filnavn og extension"
+  }
+  $dir = [System.IO.Path]::GetDirectoryName($path)
   if (-not [string]::IsNullOrWhiteSpace($dir)) {[System.IO.Directory]::CreateDirectory($dir) | out-null}
-	[System.IO.File]::Create($path) | out-null
+  [System.IO.File]::Create($path) | out-null
 }
 
 # Locate files or directories
 function locate($filter)
 {
-	if ($filter)
-	{
-		dir -r -filter $filter | Where Fullname -notmatch ".*\\.hg\\.*|.*\\generated\\.*" | Select FullName | Format-Table -wrap
-	}
-	else
-	{
-		echo "syntax: locate <file>"
-		echo "Finder filer eller directories. Brug wildcards hvis fulde filnavn ikke er kendt."
-		echo "Soeger ikke i 'hg' og 'generated' foldere."
-		echo "eks. locate *dims.*"
-	}
+  if ($filter)
+  {
+    dir -r -filter $filter | Where Fullname -notmatch ".*\\.hg\\.*|.*\\generated\\.*" | Select FullName | Format-Table -wrap
+  }
+  else
+  {
+    echo "syntax: locate <file>"
+    echo "Finder filer eller directories. Brug wildcards hvis fulde filnavn ikke er kendt."
+    echo "Soeger ikke i 'hg' og 'generated' foldere."
+    echo "eks. locate *dims.*"
+  }
 }
 
 
@@ -47,39 +47,39 @@ function loginsToday([switch]$useLogins, [int]$daysago=0)
 
 function tid([switch]$l, [int]$logindex, [switch]$help, [switch]$useLogins)
 {
-	if ($help)
-	{
-		write-host ""
-		write-host "usage: tid [-help | -l | -useLogins | <logindex>]"
-		write-host "Viser tid siden første logon idag"
-		write-host ""
-		write-host "options: "
-		write-host "-help      = Denne hjælp"
-		write-host "-l         = vis alle evententries idag og igår og deres index"
+  if ($help)
+  {
+    write-host ""
+    write-host "usage: tid [-help | -l | -useLogins | <logindex>]"
+    write-host "Viser tid siden første logon idag"
+    write-host ""
+    write-host "options: "
+    write-host "-help      = Denne hjælp"
+    write-host "-l         = vis alle evententries idag og igår og deres index"
     write-host "-useLogins = Kig kun på Logons i Secyrity loggen istedet for alle typer af events i System eventloggen."
-		write-host "<logindex> = Vis tidsinfo for bestemt loginindex."
-		write-host "             Kan bruges hvis man har arbejdet efter midnat dagen før eller lige har tjekket post hjemme inden arbejde."
-		return
-	}
-	
-	function trunc([Object]$tid, [int] $noChars)
-	{
-		return $tid.ToString().substring(0,$noChars)
-	}
+    write-host "<logindex> = Vis tidsinfo for bestemt loginindex."
+    write-host "             Kan bruges hvis man har arbejdet efter midnat dagen før eller lige har tjekket post hjemme inden arbejde."
+    return
+  }
+  
+  function trunc([Object]$tid, [int] $noChars)
+  {
+    return $tid.ToString().substring(0,$noChars)
+  }
 
-	if ($l)
-	{
-		return loginsToday -useLogins:$useLogins 1 | select-object -property index, timegenerated, entrytype
-	}
+  if ($l)
+  {
+    return loginsToday -useLogins:$useLogins 1 | select-object -property index, timegenerated, entrytype
+  }
     
-	if ($logindex -gt 0)
-	{
-		$i=(loginstoday -useLogins:$useLogins 1 | Where-Object {$_.index -eq $logindex})[0]
-	}
-	else{
-		$i = (loginsToday -useLogins:$useLogins)[0]
-	}
-	
+  if ($logindex -gt 0)
+  {
+    $i=(loginstoday -useLogins:$useLogins 1 | Where-Object {$_.index -eq $logindex})[0]
+  }
+  else{
+    $i = (loginsToday -useLogins:$useLogins)[0]
+  }
+  
   $start =$i.TimeGenerated.TimeOfDay
   $slut = (get-date).TimeOfDay
   $deltaTid = New-TimeSpan -Start $i.TimeGenerated -End (get-date)
@@ -97,17 +97,17 @@ function tid([switch]$l, [int]$logindex, [switch]$help, [switch]$useLogins)
 
 function findAll($data, [string[]]$filetypes)
 {
-	if ($filetypes) 
-	{
-		dir -rec -include $filetypes | ?{ $_.fullname -notmatch "\\obj\\?" -and $_.fullname -notmatch "\\.hg\\?" }| select-string $data
-	}
-	else
-	{
-		dir -rec -exclude *.dll, *.pdb | ?{ $_.fullname -notmatch "\\obj\\?" -and $_.fullname -notmatch "\\.hg\\?" }| select-string $data
-	}
+  if ($filetypes) 
+  {
+    dir -rec -include $filetypes | ?{ $_.fullname -notmatch "\\obj\\?" -and $_.fullname -notmatch "\\.hg\\?" }| select-string $data
+  }
+  else
+  {
+    dir -rec -exclude *.dll, *.pdb | ?{ $_.fullname -notmatch "\\obj\\?" -and $_.fullname -notmatch "\\.hg\\?" }| select-string $data
+  }
 }
 
 function ex($path=".")
 {
-	explorer $path
+  explorer $path
 }
